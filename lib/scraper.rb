3915eefb174fb225ed5def7ae2244f32e9cdad46
Kevin Britt <kbritt510@gmail.com>
2020-08-13 20:28:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,6 +22,21 @@ class Scraper
     #   course.description = post.css("p").text
     
   end
+  
+    def get_courses
+      self.get_page.css(".post")
+    end
+  
+  
+    def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
 end
 
 
