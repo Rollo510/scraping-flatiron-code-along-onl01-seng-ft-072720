416e9536296bd3f21f9a432b9ec0a6f8705b8090
Kevin Britt <kbritt510@gmail.com>
2020-08-13 19:58:03 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -6,12 +6,12 @@ attr_accessor :title, :schedule, :description
   
   
 def initialize
-  @title = title
-  @schedule = schedule
-  @description = description
   @@all << self
 end 
-  
+
+def self.all
+  @@all
+end
   
   
   
