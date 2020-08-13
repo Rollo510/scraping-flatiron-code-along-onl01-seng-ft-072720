bac39706441a0eaf8d4f2483c9611ef26c28d6e6
Kevin Britt <kbritt510@gmail.com>
2020-08-13 19:56:43 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,20 @@
+class Course
+  
+attr_accessor :title, :schedule, :description
 
+@@all = []  
+  
+  
+def initialize
+  @title = title
+  @schedule = schedule
+  @description = description
+end 
+  
+  
+  
+  
+  
+  
+end
 
