9e7afce38fef85e2d31087c5b3f89ffd24f167ed
Kevin Britt <kbritt510@gmail.com>
2020-08-13 20:17:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,6 +20,8 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/site-for-scraping/courses"))
     doc.css(".post").first.css("h2").text
+    doc.css(".post").first.css(".date").text
+    doc.css(".post").first.css("p").text
   end
 end
 
